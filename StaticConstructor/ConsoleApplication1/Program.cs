931a169cd520f28f2ba30d2843eaadd96e6dac8c
l.hovsepyan <l.hovsepyan@MIC-INNO04>
2016-12-27 17:05:34 +0400

    Firs changes

---
+++ StaticConstructor/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Own
+    {
+        public static  DateTime start;
+        public static int a;
+        static Own()
+        {
+            start = DateTime.Now;
+            Console.WriteLine(start);
+        }
+        public Own(int a1)
+        {
+            a = a1;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Own own = new Own(10);
+           
+        }
+    }
+}
